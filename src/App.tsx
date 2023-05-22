cc6ffd998ecc2c39f9caa1d4319e9475910f83a7
marco debernardi <marco.debernardi.1982@gmail.com>
2023-05-22 16:09:20 +0200

    start moving article in a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -20,14 +20,15 @@ function App() {
     for(let i=0; i < 10; i++) {
       arrArticles.push(fakeArticle);
     }
-
     setFakeList(arrArticles);
   }
   
-  const toggleShowNews = (value: boolean) => {
-    setShowNews(value);
+  const toggleShowNews = () => {
+    console.log("showNews", showNews);
+    setShowNews(!showNews);
   }
-
+  
+  console.log("showNews", showNews);
   console.log("listOfArticles: ", listOfArticles);
 
 
@@ -43,26 +44,22 @@ function App() {
       </header>
       <section className='section'>
         <section className='section-header'>
-          <h3>Magical</h3>
-          <select>
-            <option value="true" onChange={()=> toggleShowNews(true)}>Show news</option>
-            <option value="false" onChange={()=> toggleShowNews(false)}>Hide</option>
-          </select>
+        <button className='section-selector' value="Magical" onClick={()=> toggleShowNews()}>Magical</button>
         </section>
         {showNews ?
           <section className='section-news'>
-          {//listOfArticles.filter((a: IArticle) => a.tags.includes("magical")).map((item: IArticle, index: number) => {
-            fakeList.map((item: IArticle, index: number) => {
+          {//listOfArticles.filter((a: IArticle) => a.tags.includes("magical")).map((item: IArticle, postIndex: number) => {
+            fakeList.map((item: IArticle, postIndex: number) => {
             return (
-              <section key={index} className='singleNews'>
+              <section key={postIndex} className='singleNews'>
                 <h5 className='article-title'>{item.title}</h5>
                 <div></div>
                 <article className='article-body'>{item.body}</article>
                 <div></div>
                 <footer className='footer-tag'>
-                  {item.tags.map((tag) => {
+                  {item.tags.map((tag: string, tagIndex: number) => {
                     return (
-                      <pre className='article-tags'>{tag} </pre>
+                      <pre key={tagIndex} className='article-tags'>{tag} </pre>
                     )})
 
                   }
@@ -78,8 +75,7 @@ function App() {
         <section className='section-header'>
           <h3>Mystery</h3>
           <select>
-            <option value="true" onChange={()=> toggleShowNews(true)}>Show news</option>
-            <option value="false" onChange={()=> toggleShowNews(false)}>Hide</option>
+            <option value="true" onChange={()=> toggleShowNews()}>Show news</option>
           </select>
         </section>
         {showNews ?
@@ -113,8 +109,7 @@ function App() {
       <section className='section-header'>
           <h3>Crime</h3>
           <select>
-            <option value="true" onChange={()=> toggleShowNews(true)}>Show news</option>
-            <option value="false" onChange={()=> toggleShowNews(false)}>Hide</option>
+            <option value="true" onChange={()=> toggleShowNews()}>Show news</option>
           </select>
         </section>
         {showNews ?
@@ -148,8 +143,7 @@ function App() {
       <section className='section-header'>
           <h3>Fiction</h3>
           <select>
-            <option value="true" onChange={()=> toggleShowNews(true)}>Show news</option>
-            <option value="false" onChange={()=> toggleShowNews(false)}>Hide</option>
+            <option value="true" onChange={()=> toggleShowNews()}>Show news</option>
           </select>
         </section>
         {showNews ?
@@ -183,8 +177,7 @@ function App() {
         <section className='section-header'>
           <h3>History</h3>
           <select>
-            <option value="true" onChange={()=> toggleShowNews(true)}>Show news</option>
-            <option value="false" onChange={()=> toggleShowNews(false)}>Hide</option>
+            <option value="true" onChange={()=> toggleShowNews()}>Show news</option>
           </select>
         </section>
         {showNews ?
---
+++ src/component/Article.tsx
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+
+
+function Article() {
+
+}
+
+export default Article;
